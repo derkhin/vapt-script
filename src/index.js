533f89499b8386d95f1466f53eee013805e23e00
derkhin <mr.sayyedirfan@gmail.com>
2023-08-25 00:08:23 +0530

    day2 for vapt script

--- src/index.js
+++ src/index.js
@@ -5,22 +5,26 @@ const handlebars = require('handlebars');
 async function generateHTML(jsonData) {
     const templatePath = path.join(__dirname, 'templates/template.hbs');
     const templateContent = await fs.readFile(templatePath, 'utf-8');
-
     const compiledTemplate = handlebars.compile(templateContent);
-    const html = compiledTemplate(jsonData);
+    const html = compiledTemplate({ data: jsonData });
 
     const htmlFilePath = path.join(__dirname, 'generated.html');
     await fs.writeFile(htmlFilePath, html, 'utf-8');
     console.log('HTML file generated successfully.');
 }
 
-async function fetchJSONAndStore() {
-    const jsonFileName = 'reports.json';
-    const jsonFilePath = path.join(__dirname, jsonFileName);
-
+async function convertTextToJSON(file_name) {
+    const reportFileName = path.join(__dirname, `reports/${file_name}.txt`);
     try {
-        const jsonData = await fs.readFile(jsonFilePath, 'utf-8');
-        return JSON.parse(jsonData);
+        const reportData = await fs.readFile(reportFileName, 'utf-8');
+        const lines = reportData.trim().split('\n');
+        const entity = [];
+        lines.forEach((line, index) => {
+            const strippedLine = line.trim();
+            entity.push({ value: strippedLine, index: index + 1 });
+        });
+        console.log('[+] entity')
+        return entity;
     } catch (error) {
         console.error('Error:', error);
         return null;
@@ -28,9 +32,15 @@ async function fetchJSONAndStore() {
 }
 
 (async () => {
-    const jsonData = await fetchJSONAndStore();
-    if (jsonData) {
-        console.log(jsonData)
-        await generateHTML(jsonData)
+    let files = ['subdomains', 'aliveIp', 'HostPort'];
+    const solidData = await Promise.all(files.map(async (file_name) => {
+        return {
+            [file_name]: await convertTextToJSON(file_name)
+        };
+    }));
+    solidData.unshift({ "sidebar": files })
+    if (solidData.length) {
+        console.log(JSON.stringify(solidData))
+        await generateHTML(solidData)
     }
 })();
